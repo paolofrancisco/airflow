01338fe6fba905f25199b002510950cf7fc41e66
Paolo <paulofrancisco88@gmail.com>
2024-04-16 00:34:04 +0800

    initial commit

---
+++ dags/hello_world.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG object
+dag = DAG(
+    'hello_world',
+    default_args=default_args,
+    description='A simple DAG that prints Hello, World!',
+    schedule_interval='@daily',
+)
+
+# Define the tasks
+task_hello_world = BashOperator(
+    task_id='print_hello_world',
+    bash_command='echo "Hello, World!"',
+    dag=dag,
+)
+
+# Define task dependencies
+task_hello_world
